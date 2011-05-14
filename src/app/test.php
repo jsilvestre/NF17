b28dcf649d900ec0fb1eac3be6a50e2211119e5a
unknown <Paco@.(none)>
2011-05-14 15:34:00 +0200

    Ajout d'un fichier de test. Suppression de fichiers inutiles.

---
+++ src/app/test.php
@@ -0,0 +1,15 @@
+<div id="wrapper">
+	<div class="box">
+		<h2>Accueil</h2>
+		<p>Page principale</p>
+		<p> copucou</p>
+	</div>
+</div>
+
+<div id="action">
+	<h2>Actions possibles</h2>
+	<ul>
+		<li><a href="index.php">Item 1</a></li>
+		<li><a href="index.php">Item 2</a></li>
+	</ul>
+</div>
