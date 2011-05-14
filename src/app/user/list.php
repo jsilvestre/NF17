3a1f4da0ae6e07169c30631ae348163380e10127
Joseph Silvestre <contact@jsilvestre.fr>
2011-05-14 23:19:57 +0200

    refactorining du code

--- src/app/user/list.php
+++ src/app/user/list.php
@@ -1,35 +1,39 @@
-<div id="wrapper">
-	<div class="box">
-		<h2>Accueil</h2>
-		<?php
-		$pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
-		$bdd = new PDO('mysql:host=localhost;dbname=nf17', 'root', '', $pdo_options);
-		// En cas de premiere page, on affiche la liste des utilisateurs dans la base de données
-		if(empty($_GET['id']))
+<?php
+
+	// initialise la variable d'affichage
+	$display = '';
+
+	// En cas de premiere page, on affiche la liste des utilisateurs dans la base de donnÃ©es
+	if(empty($_GET['id']))
+	{
+		$req=$db->prepare('SELECT login from utilisateur');
+		// Execution de la requete
+		$req->execute();
+		while($result = $req->fetch(PDO::FETCH_ASSOC))
 		{
-			$req=$bdd->prepare('SELECT login from utilisateur');
-			// Execution de la requete
-			$req->execute();
-			while($result = $req->fetch(PDO::FETCH_ASSOC))
-			{
-				echo "<a href=index.php?page=./user/list&id=",$result['login'],">",$result['login'],"</a>";
-				echo "<BR>";
-			}
-			$req->closeCursor();
+			$display.= '<a href="index.php?page=./user/list&id='.$result['login'].'">'.$result['login'].'</a>';
+			$display.= "<br />";
 		}
-		else // Si un utilisateur a ete selectionne, on affiche la liste des informations le concernant
-		{
+		$req->closeCursor();
+	}
+	else // Si un utilisateur a ete selectionne, on affiche la liste des informations le concernant
+	{
 		$req=$bdd->prepare("select * from utilisateur where login=?");
 		$req->execute(array($_GET['id']));
 		$result = $req->fetch(PDO::FETCH_ASSOC);
-		echo "nom d utilisateur : ",$result['login'],"<BR>" ;
-		echo "nom  : ",$result['nom'],"<BR>" ;
-		echo "Prenom : ",$result['prenom'],"<BR>" ;
-		echo "Date de naissance : ",$result['dateNaissance'],"<BR>" ;
-		echo "Numero de securite sociale : ",$result['numSS'],"<BR> <BR><BR><BR>" ;
-		echo "<a href=index.php?page=./user/list> Retour </a>";
-		}
-	?>
+		$display.= "nom d utilisateur : ".$result['login']."<br />" ;
+		$display.= "nom  : ".$result['nom']."<br />" ;
+		$display.= "Prenom : ".$result['prenom']."<br />" ;
+		$display.= "Date de naissance : ".$result['dateNaissance']."<br />" ;
+		$display.= "Numero de securite sociale : ".$result['numSS']."<br /> <br /><br /><br />" ;
+		$display.= '<a href="index.php?page=user/list"> Retour </a>';
+	}
+?>
+
+<div id="wrapper">
+	<div class="box">
+		<h2>Accueil</h2>
+		<p><?php echo $display; ?></p>
 	</div>
 </div>
 
