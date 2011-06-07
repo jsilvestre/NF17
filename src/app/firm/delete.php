ea0c0f1829223207eb028b928612bdda09c87ec6
unknown <Paco@.(none)>
2011-06-07 17:10:39 +0200

    ptite modif

--- src/app/firm/delete.php
+++ src/app/firm/delete.php
@@ -5,15 +5,23 @@ if(!empty($_GET['id']))
 	$req=$db->prepare("select nom from organisation where nom=?");
 	$req->execute(array($_GET['id']));
 	
-	if($req->rowCount() == 0)
+	if($req->rowCount() == 0) // On verifie que l'organisation existe bien
 	{
 		header("Location: index.php?page=general/message&type=error&msg=Organisation introuvable.&retour=firm/list");
 	}
 	else
 	{
+		// Suppression des adresses
+		$req=$db->prepare("DELETE FROM adresse WHERE organisation=?");
+		$req->execute(array($_GET['id']));
+		$req->closeCursor();
+		
+		// Suppression de l'organisation
 		$req=$db->prepare("DELETE FROM organisation WHERE nom=?");
 		$req->execute(array($_GET['id']));
-		header("Location: index.php?page=general/message&type=confirm&msg=L'organisation a bien été supprimé.&retour=firm/list");
+		$req->closeCursor();
+				
+		header("Location: index.php?page=general/message&type=confirm&msg=L'organisation a bien été supprimé.&retour=firm/list");		
 	}
 	
 }
